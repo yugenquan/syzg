9e977c3420c87c80391c934a08ec416a7fb749b4
漫步人间 <1064728163@qq.com>
2019-10-17 15:20:29 +0800

    上传项目

---
+++ src/main/java/cn/com/syzg/service/EmplyService.java
@@ -0,0 +1,5 @@
+package cn.com.syzg.service;
+
+public interface EmplyService {
+
+}
---
+++ src/main/java/cn/com/syzg/service/Impl/EmplyServiceImpl.java
@@ -0,0 +1,8 @@
+package cn.com.syzg.service.Impl;
+
+import cn.com.syzg.service.EmplyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmplyServiceImpl  implements EmplyService {
+}
